da9cbcbdff01a9bfdb32741037d174155c124a94
Matus Zelenak <mato.zelenak@gmail.com>
2021-05-10 23:11:18 +0200

    inserting instructions, call function

--- plugin-v3/cfcss.c
+++ plugin-v3/cfcss.c
@@ -10,6 +10,7 @@
 
 int plugin_is_GPL_compatible = 1;
 int *signatures, *differences, *adjusting_signatures;
+tree cfcss_error_handler = NULL;
 
 static struct plugin_info plugin_info =
 {
@@ -204,28 +205,27 @@ void handle_aliasing()
 
 void insert_instructions()
 {
-  basic_block bb, bb_first, bb_correct, bb_err;
+  basic_block bb, bb_first, bb_correct, bb_correct_first, bb_err;
   tree G, D, dj, sj, adj_sig, lbl_correct, lbl_err;
-  gimple stmt_assign, stmt_nop, stmt_cond;
-  gimple_stmt_iterator gsi, si;
+  gimple stmt_assign, stmt_nop, stmt_cond, stmt_call;
+  gimple_stmt_iterator gsi, gsi_err;
   edge e, e_end, e_correct, e_err;
   int bb_index, i;
+  int n_original_bbs;
 
+  n_original_bbs = n_basic_blocks;
+
+  // create runtime variables for inserting
   G = create_tmp_var (integer_type_node, "G");
   D = create_tmp_var (integer_type_node, "D");
   DECL_INITIAL (G) = build_int_cst (integer_type_node, signatures[0]);
-  DECL_INITIAL (D) = build_int_cst (integer_type_node, 1000);
+  DECL_INITIAL (D) = build_int_cst (integer_type_node, signatures[0]);
   mark_sym_for_renaming(G);
   mark_sym_for_renaming(D);
 
+  // create constants for inserting
   dj = build_int_cst (integer_type_node, differences[0]);
   sj = build_int_cst (integer_type_node, signatures[0]);
-
-  // bb_first = get_bb_by_index(2);
-  // gsi = gsi_start_bb(bb_first);
-  // stmt_nop = gimple_build_nop();
-  // update_stmt(stmt_nop);
-  // gsi_insert_before (&gsi, stmt_nop, GSI_NEW_STMT);
 
   // create error handling block and handle first block
   bb_first = get_bb_by_index(2);
@@ -241,11 +241,6 @@ void insert_instructions()
     gsi_insert_after (&gsi, stmt_assign, GSI_NEW_STMT);
   }
 
-  // for (i = 0; i < n_basic_blocks; i++) {
-  //   printf("%d ", adjusting_signatures[i]);
-  // }
-  // printf("\n");
-
   if (adjusting_signatures[index_bb(bb_first)] != -1) {
     adj_sig = build_int_cst (integer_type_node, adjusting_signatures[index_bb(bb_first)]);
     stmt_assign = gimple_build_assign(D, adj_sig);
@@ -253,19 +248,22 @@ void insert_instructions()
     gsi_insert_after (&gsi, stmt_assign, GSI_NEW_STMT);
   }
 
-
   e_correct = split_block (bb_first, gsi_stmt (gsi));
   execute_on_growing_pred (e_correct);
-  bb_correct = e_correct->dest;
+  bb_correct_first = e_correct->dest;
 
   e_err = split_block (bb_first, gsi_stmt (gsi));
   execute_on_growing_pred (e_err);
   bb_err = e_err->dest;
 
+  gsi_err = gsi_start_bb(bb_err);
+  stmt_call = gimple_build_call (cfcss_error_handler, 0);
+  gsi_insert_after (&gsi_err, stmt_call, GSI_NEW_STMT);
+
   execute_on_shrinking_pred (e_correct);
   remove_edge(e_correct);
 
-  e_correct = make_edge (bb_first, bb_correct, EDGE_TRUE_VALUE);
+  e_correct = make_edge (bb_first, bb_correct_first, EDGE_TRUE_VALUE);
 
   if (e_err->flags & EDGE_FALLTHRU)
   {
@@ -276,7 +274,7 @@ void insert_instructions()
     e_err->flags = e_err->flags + EDGE_FALSE_VALUE;
   }
 
-  lbl_correct = gimple_block_label(bb_correct);
+  lbl_correct = gimple_block_label(bb_correct_first);
   lbl_err = gimple_block_label(bb_err);
   stmt_cond = gimple_build_cond (EQ_EXPR, G, sj, lbl_correct, lbl_err);
   gsi_insert_after (&gsi, stmt_cond, GSI_NEW_STMT);
@@ -285,52 +283,79 @@ void insert_instructions()
   if (dom_info_available_p (CDI_DOMINATORS))
   {
     set_immediate_dominator (CDI_DOMINATORS, bb_first, recompute_dominator (CDI_DOMINATORS, bb_first));
-    set_immediate_dominator (CDI_DOMINATORS, bb_correct, recompute_dominator (CDI_DOMINATORS, bb_correct));
+    set_immediate_dominator (CDI_DOMINATORS, bb_correct_first, recompute_dominator (CDI_DOMINATORS, bb_correct_first));
     set_immediate_dominator (CDI_DOMINATORS, bb_err, recompute_dominator (CDI_DOMINATORS, bb_err));
   }
   if (dom_info_available_p (CDI_POST_DOMINATORS))
   {
 	  set_immediate_dominator (CDI_POST_DOMINATORS, bb_first, recompute_dominator (CDI_POST_DOMINATORS, bb_first));
-    set_immediate_dominator (CDI_POST_DOMINATORS, bb_correct, recompute_dominator (CDI_POST_DOMINATORS, bb_correct));
+    set_immediate_dominator (CDI_POST_DOMINATORS, bb_correct_first, recompute_dominator (CDI_POST_DOMINATORS, bb_correct_first));
     set_immediate_dominator (CDI_POST_DOMINATORS, bb_err, recompute_dominator (CDI_POST_DOMINATORS, bb_err));
   }
 
-
-  // e_end = split_block (bb_first, gsi_stmt(gsi));
-
-  FOR_EACH_BB(bb)
+  for (i = 3; i < n_original_bbs; i++)
   {
-    // bb_index = index_bb(bb);
-    
-    // dj = build_int_cst (integer_type_node, differences[bb_index]);
+    bb = get_bb_by_index(i);
+    if (bb->index == bb_first->index || bb->index == bb_correct_first->index || bb->index == bb_err->index) {
+      continue;
+    }
+    printf("%d\n", bb->index);
 
-    // stmtassign = gimple_build_assign_with_ops(BIT_XOR_EXPR, G, G, dj); // ****NEPOZNA VER 9
+    bb_index = index_bb(bb);
+    dj = build_int_cst (integer_type_node, differences[bb_index]);
+    sj = build_int_cst (integer_type_node, signatures[bb_index]);
 
-    // gsi = gsi_start_bb(bb);
-    // gsi_insert_before (&gsi, stmtassign, GSI_SAME_STMT); // *** TOTO ROBI PROBLEM S GLOB PREMENNYMI ***
-    // update_stmt (stmtassign);
-    // gimple_block_label(bbafter);
+    gsi = gsi_start_bb(bb);
 
-    // gimple_dump_bb (bb, stderr, 0, TDF_SLIM);
+    stmt_assign = gimple_build_assign_with_ops(BIT_XOR_EXPR, G, G, dj);
+    update_stmt(stmt_assign);
+    gsi_insert_after (&gsi, stmt_assign, GSI_NEW_STMT);
 
-    // lblbb2 = gimple_block_label(bb);
-    // //stmtcond=gimple_build_cond (EQ_EXPR, spolu, v3000, lblend, lblaftercond);
-    // stmtcond=gimple_build_cond (EQ_EXPR, spolu, v3000, lblend, lblbb2);
+    if (is_branch_fan_in_node(bb)) {
+      stmt_assign = gimple_build_assign_with_ops(BIT_XOR_EXPR, G, G, D);
+      update_stmt(stmt_assign);
+      gsi_insert_after (&gsi, stmt_assign, GSI_NEW_STMT);
+    }
+
+    if (adjusting_signatures[bb_index] != -1) {
+      adj_sig = build_int_cst (integer_type_node, adjusting_signatures[bb_index]);
+      stmt_assign = gimple_build_assign(D, adj_sig);
+      update_stmt(stmt_assign);
+      gsi_insert_after (&gsi, stmt_assign, GSI_NEW_STMT);
+    }
+
+    e_correct = split_block (bb, gsi_stmt (gsi));
+    execute_on_growing_pred (e_correct);
+    bb_correct = e_correct->dest;
+
+    e_err = make_edge (bb, bb_err, EDGE_FALSE_VALUE);
+
+    if (e_correct->flags & EDGE_FALLTHRU)
+    {
+      e_correct->flags = e_correct->flags - EDGE_FALLTHRU;
+    }
+    if (!(e_correct->flags & EDGE_FALSE_VALUE))
+    {
+      e_correct->flags = e_correct->flags + EDGE_TRUE_VALUE;
+    }
+
+    lbl_correct = gimple_block_label(bb_correct);
+    stmt_cond = gimple_build_cond (EQ_EXPR, G, sj, lbl_correct, lbl_err);
+    gsi_insert_after (&gsi, stmt_cond, GSI_NEW_STMT);
 
 
-    // gsi_insert_before (&gsi, stmtcond, GSI_SAME_STMT);
-    // update_stmt (stmtcond);
-    // gimple_dump_bb (bb, stderr, 0, TDF_SLIM);
-    // gimple_stmt_iterator si; 
-    // for (si = gsi_start_bb (bb); !gsi_end_p (si); gsi_next (&si))
-    // {
-    //   gimple stmt = gsi_stmt (si);
-      
-    //   for (i=0; i<gimple_num_ops(stmt); ++i)
-    //   {
-    //     op = gimple_op(stmt, i); 
-    //   }
-    // }
+    if (dom_info_available_p (CDI_DOMINATORS))
+    {
+      set_immediate_dominator (CDI_DOMINATORS, bb, recompute_dominator (CDI_DOMINATORS, bb));
+      set_immediate_dominator (CDI_DOMINATORS, bb_correct, recompute_dominator (CDI_DOMINATORS, bb_correct));
+      set_immediate_dominator (CDI_DOMINATORS, bb_err, recompute_dominator (CDI_DOMINATORS, bb_err));
+    }
+    if (dom_info_available_p (CDI_POST_DOMINATORS))
+    {
+	    set_immediate_dominator (CDI_POST_DOMINATORS, bb, recompute_dominator (CDI_POST_DOMINATORS, bb));
+      set_immediate_dominator (CDI_POST_DOMINATORS, bb_correct, recompute_dominator (CDI_POST_DOMINATORS, bb_correct));
+      set_immediate_dominator (CDI_POST_DOMINATORS, bb_err, recompute_dominator (CDI_POST_DOMINATORS, bb_err));
+    }
   }
 }
 
@@ -347,8 +372,17 @@ static unsigned plugin_exec(void)
     ALL_BB_print();
     // dumpcfg();
 
-    if (n_basic_blocks <= 1) {
-      printf("Function with only one BB, therefore not implementing CFCSS method\n");
+    // if (n_basic_blocks <= 1) {
+    //   printf("Function with only one BB, therefore not implementing CFCSS method\n");
+    //   return 0;
+    // }
+
+    if (get_identifier(current_function_name()) == get_identifier("cfcss_error_handler"))
+    {
+      if (cfcss_error_handler == NULL)
+      {
+        cfcss_error_handler = current_function_decl;
+      }
       return 0;
     }
 
--- test-files/test-cfcss.c
+++ test-files/test-cfcss.c
@@ -5,17 +5,33 @@ int b[3] = {2, 3, 2};
 int x[3];
 int i = 0;
 
+void cfcss_error_handler() {
+    printf("CFCSS detected CFE, exiting program...\n");
+    exit(1);
+}
+
+void print_line() {
+    printf("Hello world!\n");
+}
+
 int main () {
+    printf("1\n");
     while (i < 3) {
+        printf("2\n");
         if (a[i] < b[i]) {
+            printf("3\n");
             x[i] = a[i];
         }
         else {
+            printf("4\n");
             x[i] = b[i];
         }
+        printf("5\n");
         i++;
     }
-
+    printf("6\n");
+    print_line();
+    printf("7\n");
     return 0;
 }
 
